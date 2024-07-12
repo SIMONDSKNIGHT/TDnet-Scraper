d5ae44765266314e59a7e6a556bb06217f90d4a7
SIMONDSKNIGHT <knigsd@icloud.com>
2024-07-12 14:54:46 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,7 @@ BASE_URL = 'https://www.release.tdnet.info/onsf/TDJFSearch/TDJFSearch'
 DOWNLOAD_PATH = './downloads'
 
 HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Encoding": "gzip, deflate, br, zstd",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
     "Cache-Control": "max-age=0",
